773713ff4a6a2d1c20f93750797637fffdad4869
Zerubbabel <zerubt16@gmail.com>
2026-03-26 18:28:49 +0300

    feat: add VS Code-like git diff (gitsigns, diffview) and UI cleanup

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,5 +1,7 @@
 local function enable_transparency()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 return {
 	{
